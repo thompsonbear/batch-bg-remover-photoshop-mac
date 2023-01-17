286cc0bc4a6a9c6a2201103b2a330c0194a318b6
Thompson Bear <thompsonbear21@gmail.com>
2023-01-17 15:30:17 -0500

    Updated PC paths to be folders on the desktop of a Mac

--- remove-bg.jsx
+++ remove-bg.jsx
@@ -6,16 +6,15 @@ This scripts supports Photoshop CC 2020 and above versions.
 How to use the script: https://youtu.be/6ICVsi2pWyk
 --------------------------------------------------------------------------------------*/
 
-
 /*------------------------------------------------------------------------------------
 Configure following paramers before running the script
 --------------------------------------------------------------------------------------*/
 //Place all images needs to be processed in a folder. Add the path below.
-var sourceFolder = Folder("C:\\ps\\src");
+var sourceFolder = Folder("~/Desktop/src");
 //Add the path of an existing folder below to save the output.
-var saveFolder = new Folder("C:\\ps\\out");
+var saveFolder = new Folder("~/Desktop/out");
 //Fill color of the background
-var colorRef = new SolidColor;
+var colorRef = new SolidColor();
 colorRef.rgb.red = 255;
 colorRef.rgb.green = 255;
 colorRef.rgb.blue = 255;
@@ -28,79 +27,79 @@ var isImageBg = true;
 //Backdound image must be the active document
 //-----------------------------------------------------------------------------------
 
-
 //Check if it's selected to use an image as background
-	if(isImageBg){
-		//Store background image and a variable
-		var doc_bg = app.activeDocument;
-	}
-
+if (isImageBg) {
+  //Store background image and a variable
+  var doc_bg = app.activeDocument;
+}
 
 //Cheks if the source folder is null
-  if (sourceFolder != null)
-  {
-	//The following line will list all files (not only image files) on the source folder.
-	//If you have any non-image files (even hidden) , please see the next comment.
-    	//var fileList = sourceFolder.getFiles();
-	//Comment the above line and uncomment the following line to filter specific file types.
-	//Try filter files types if the script fails.
-	var fileList = sourceFolder.getFiles(/\.(jpg|jpeg|png|tif|psd|crw|cr2|nef|dcr|dc2|raw|heic)$/i);
-  }
-  else{
-	  alert("No images found on source folder");
-  }
-
+if (sourceFolder != null) {
+  //The following line will list all files (not only image files) on the source folder.
+  //If you have any non-image files (even hidden) , please see the next comment.
+  //var fileList = sourceFolder.getFiles();
+  //Comment the above line and uncomment the following line to filter specific file types.
+  //Try filter files types if the script fails.
+  var fileList = sourceFolder.getFiles(
+    /\.(jpg|jpeg|png|tif|psd|crw|cr2|nef|dcr|dc2|raw|heic)$/i
+  );
+} else {
+  alert("No images found on source folder");
+}
 
 //Now this will open every file in the file list
-for(var a = 0 ;a < fileList.length; a++){
-	//Open file in photoshop    
-	app.open(fileList[a]);
+for (var a = 0; a < fileList.length; a++) {
+  //Open file in photoshop
+  app.open(fileList[a]);
 
-	// Select subject
-	var idautoCutout = stringIDToTypeID( "autoCutout" );
-	var desc01 = new ActionDescriptor();
-	var idsampleAllLayers = stringIDToTypeID( "sampleAllLayers" );
-	desc01.putBoolean( idsampleAllLayers, false );
-	try{
-		executeAction( idautoCutout, desc01, DialogModes.NO );
-	}
-	catch(err){}
-	// Invert the selection
-	app.activeDocument.selection.invert();
+  // Select subject
+  var idautoCutout = stringIDToTypeID("autoCutout");
+  var desc01 = new ActionDescriptor();
+  var idsampleAllLayers = stringIDToTypeID("sampleAllLayers");
+  desc01.putBoolean(idsampleAllLayers, false);
+  try {
+    executeAction(idautoCutout, desc01, DialogModes.NO);
+  } catch (err) {}
+  // Invert the selection
+  app.activeDocument.selection.invert();
 
+  //Now the background is selected. Next step is to fill or clear the selection.
+  if (isTransparent) {
+    //Make active layer a normal layer.
+    activeDocument.activeLayer.isBackgroundLayer = false;
+    //Make the selection transparent
+    app.activeDocument.selection.clear();
+  } else {
+    app.activeDocument.selection.fill(colorRef);
+  }
 
-	//Now the background is selected. Next step is to fill or clear the selection.
-	if(isTransparent){
-		//Make active layer a normal layer.
-		activeDocument.activeLayer.isBackgroundLayer = false;
-		//Make the selection transparent
-		app.activeDocument.selection.clear();
-	}
-	else{
-		app.activeDocument.selection.fill(colorRef);
-	}
-	
-	
-	//Check if it's selected to use an image as background
-	if(isImageBg){
-		//Store main document to a variable
-		var main_doc = app.activeDocument;
-		//Swich to background image
-		app.activeDocument = doc_bg;
-		//Copy background to the main image
-		app.activeDocument.activeLayer.duplicate(main_doc, ElementPlacement.PLACEATEND);
-		//Switch to the main image
-		app.activeDocument = main_doc;
-	}
-	
+  //Check if it's selected to use an image as background
+  if (isImageBg) {
+    //Store main document to a variable
+    var main_doc = app.activeDocument;
+    //Swich to background image
+    app.activeDocument = doc_bg;
+    //Copy background to the main image
+    app.activeDocument.activeLayer.duplicate(
+      main_doc,
+      ElementPlacement.PLACEATEND
+    );
+    //Switch to the main image
+    app.activeDocument = main_doc;
+  }
 
-	//Now the image is proccessed. Next step is saving the image.
-	//Create the file name
-	var fileName = app.activeDocument.name.replace(/\.[^\.]+$/, ''); 
-	pngSaveOptions = new PNGSaveOptions();
-	//Edit png options here.
-	//Save image as PNG
-	app.activeDocument.saveAs(new File(saveFolder +'/'+ Date.now() + "_" + fileName + '.png'), pngSaveOptions, true, Extension.LOWERCASE);
-	//Close image whithout saving as PSD
-	app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+  //Now the image is proccessed. Next step is saving the image.
+  //Create the file name
+  var fileName = app.activeDocument.name.replace(/\.[^\.]+$/, "");
+  pngSaveOptions = new PNGSaveOptions();
+  //Edit png options here.
+  //Save image as PNG
+  app.activeDocument.saveAs(
+    new File(saveFolder + "/" + Date.now() + "_" + fileName + ".png"),
+    pngSaveOptions,
+    true,
+    Extension.LOWERCASE
+  );
+  //Close image whithout saving as PSD
+  app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
 }
